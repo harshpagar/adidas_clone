32f0b9f458e7723ed28b4de82d8792ac984c9b52
Harshal  Pagar <72097667+harshpagar@users.noreply.github.com>
2022-04-24 02:33:26 +0530

    update

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -19,23 +19,17 @@ export const Billing = () =>
         <h2>Billing</h2>
         <strong>Credit or Debit Card</strong>
         <br/> <br/>
-        <div className="paymentfields">        
-            <Box
-                sx={{
-                    width: 500,
-                    maxWidth: '100%',
-                    marginLeft: "35%"
-                }}
-                >
-                <TextField fullWidth label="Enter Full Name : " id="fullWidth" />
+        <div style={{height:"400px",borderColor:"black",border: "1px solid black",width:"600px",marginTop:"40px",marginLeft:"450px"}}>        
+            <Box>
+                <TextField style={{width:"500px",marginTop:"50px"}} fullWidth label="Enter Full Name : " id="fullWidth" />
                 <br /><br />
-                <TextField fullWidth label="Enter Cart Number : " id="fullWidth" /> 
+                <TextField style={{width:"500px"}} fullWidth label="Enter Cart Number : " id="fullWidth" /> 
                 <br />      
             </Box>
             
                 <div>
-                <Box>
-                    <TextField style={{marginTop:"15px"}}  id="demo-helper-text-misaligned-no-helper" label="Expiry : " />
+                <Box >
+                    <TextField style={{marginTop:"15px",marginRight:"50px"}}  id="demo-helper-text-misaligned-no-helper" label="Expiry : " />
                     <TextField style={{marginTop:"15px"}} id="demo-helper-text-misaligned-no-helper" label="CVV : " />
                     </Box>
                 </div>  

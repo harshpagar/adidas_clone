ebc7123ebdf3439ca8f8b67fd45452dbf58b0cb6
Harshal  Pagar <72097667+harshpagar@users.noreply.github.com>
2022-06-06 10:13:34 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,34 +1,29 @@
-import './App.css';
-import { Home } from './components/Home';
+import "./App.css";
+import { Home } from "./components/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Products} from './components/Products'
-import { Cart } from './components/Cart'
-import { Billing } from './components/Billing'
-import { Login } from './components/Login'
-import { SignUp } from './components/SignUp'
-import { UserDetails } from './components/UserDetails';
-
+import { Products } from "./components/Products";
+import { Cart } from "./components/Cart";
+import { Billing } from "./components/Billing";
+import { Login } from "./components/Login";
+import { SignUp } from "./components/SignUp";
+import { UserDetails } from "./components/UserDetails";
 
 function App() {
   return (
-
-  <div className ="App">
-   {/* <Navbar/>  */}
-        <BrowserRouter>
-          <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/Products" element={<Products />} />
-              <Route path="/cart" element={< Cart/>} />
-              <Route path="/billing" element={< Billing/>} />
-              <Route path="/login" element={< Login/>} />
-              <Route path="/signUp" element={< SignUp/>} />
-              <Route path="/user" element={< UserDetails />} /> 
-          </Routes>
-        </BrowserRouter>
-
-  </div>
-
-   
+    <div className="App">
+      {/* <Navbar/>  */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Products" element={<Products />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/billing" element={<Billing />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signUp" element={<SignUp />} />
+          <Route path="/user" element={<UserDetails />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
